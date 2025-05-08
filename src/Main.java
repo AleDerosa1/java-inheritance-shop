cfa9ca320393a183c34f27d45ece1c4e26a0f4cf
AleDerosa1 <derosaalessandro018@gmail.com>
2025-05-09 00:35:51 +0200

    continuo pt2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import org.lessons.java.shop.Prodotto;
+import org.lessons.java.shop.Smartphone;
 
 public class Main {
     public static void main(String[] args) {
 
-        
+        Smartphone Iphone = new Smartphone("Iphone", "Apple", 1200, 22, 3784600, 128);
+
+        System.out.println(Iphone.getNome());
+
+
+
+
         Prodotto gioco = new Prodotto("FC24", "EA", 79.9f, 22);
         System.out.println("Codice: " + gioco.getCodice());
         System.out.println("Nome Gioco: " + gioco.getNome());
         System.out.println("Marca: " + gioco.getMarca());
         System.out.println("Prezzo: " + gioco.getPrezzo() + "$");
         System.out.println("Prezzo con IVA: " + gioco.getPrezzoIva() + "$");
-
         System.out.println(gioco.getNomeCodice());
-        
-
-    }
-        
 
     }
 
+}
--- src/org/lessons/java/shop/Smartphone.java
+++ src/org/lessons/java/shop/Smartphone.java
@@ -2,7 +2,26 @@ package org.lessons.java.shop;
 
 public class Smartphone extends Prodotto {
 
-    public int codiceIMEI;
+     public int codiceIMEI;
     public int memoria;
+
+    public Smartphone(String nome, String marca, float prezzo, float iva, int codiceIMEI, int memoria){
+
+        //INVOCA IL COSTRUTTORE DELLA MIA SUPERCLASSE
+        super(nome , marca , prezzo , iva);
+
+        this.codiceIMEI = codiceIMEI;
+        this.memoria = memoria;
+
+    }
+
+   public int getCodiceIMEI(){
+    return this.codiceIMEI;
+   }
+
+   public int getMemoria(){
+    return this.memoria;
+   }
+   
     
 }
